7779a33563a6a8effabe3f0d85ad9a2b19c0a440
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-02 18:17:06 +0000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  characters = string.split("");
+  characters = characters.reverse();
+  return characters.join("");
 };
 
 // Do not edit below this line
