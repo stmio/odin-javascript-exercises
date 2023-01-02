5959ec014dca59d9dac8f198bb9fb47383202511
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-02 18:53:38 +0000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+  for (const item of items) {
+    let index = array.indexOf(item);
+    if (index != -1) array.splice(index, 1);
+  }
 
+  return array;
 };
 
 // Do not edit below this line
