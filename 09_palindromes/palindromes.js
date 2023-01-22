b6e5781a6a5b54bac823a1de216c6bfa74280863
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-22 10:46:51 +0000

    Complete exercises 9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const letters = string.toLowerCase().replace(/[^a-z]/g, "");
+  return letters.split("").reverse().join("") === letters;
 };
 
 // Do not edit below this line
