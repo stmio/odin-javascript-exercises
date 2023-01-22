651bdfe168d33d8f5044d7fee95a7dac0d3f6a2c
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-22 12:52:25 +0000

    Complete exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce(
+    (previous, current) => {
+      if (!current.yearOfDeath) {
+        current.age = new Date().getFullYear() - current.yearOfBirth;
+      } else current.age = current.yearOfDeath - current.yearOfBirth;
 
+      if (previous.age < current.age) {
+        return current;
+      } else return previous;
+    },
+    { age: 0 }
+  );
 };
 
 // Do not edit below this line
